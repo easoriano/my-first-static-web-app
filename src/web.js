00d44952905fc4302b6bc6d557d9a2c07fa27b75
Easoriano <39744276+easoriano@users.noreply.github.com>
2023-03-07 21:02:21 -0800

    Update web.js

--- src/web.js
+++ src/web.js
@@ -49,7 +49,7 @@ function diabeties()
       
 }
 */
-const data = {
+/*const data = {
   "Inputs": {
     "input1": [
       {
@@ -88,3 +88,61 @@ fetch('http://20.252.33.64/api/v1/service/predict-diabetes/score', options)
   .catch(error => {
     console.error(error);
   });
+*/
+
+const https = require('https');
+const querystring = require('querystring');
+
+const data =  {
+  "Inputs": {
+    "input1": [
+      {
+        "PatientID": 1882185,
+        "Pregnancies": 9,
+        "PlasmaGlucose": 104,
+        "DiastolicBloodPressure": 51,
+        "TricepsThickness": 7,
+        "SerumInsulin": 24,
+        "BMI": 27.36983156,
+        "DiabetesPedigree": 1.3504720469999998,
+        "Age": 43
+      }
+    ]
+  },
+  "GlobalParameters": {}
+};
+
+const postData = JSON.stringify(data);
+
+const options = {
+  hostname: '20.252.33.64',
+  port: 80,
+  path: '/api/v1/service/predict-diabetes/score',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': postData.length,
+    'Authorization': 'Bearer ' + 'crDbBjiIZb2mgOGcJQnT7BPFsR5R64QZ',
+  }
+};
+
+const req = https.request(options, (res) => {
+  console.log(`statusCode: ${res.statusCode}`);
+
+  let result = '';
+
+  res.on('data', (d) => {
+    result += d;
+  });
+
+  res.on('end', () => {
+    document.getElementById('results').innerHTML = result;
+  });
+});
+
+req.on('error', (error) => {
+  console.error(error);
+});
+
+req.write(postData);
+req.end();
